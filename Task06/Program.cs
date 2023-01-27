1600685ca18ec5098fa37a8d4d1625a0f6ff9535
Anans <19ananasik9595@mail.ru>
2023-01-27 03:34:43 +0300

    Task06 исправлены ошибки формулировки ответа

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -9,9 +9,9 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 if (num % 2 == 1)
 {
-    Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+    Console.WriteLine("Нет");
 }
 else
 {
-    Console.WriteLine("Число " + num + "является: ЧЁТНЫМ");
+    Console.WriteLine("Да");
 }
