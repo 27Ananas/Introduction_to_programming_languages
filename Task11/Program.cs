7664987572ff7e1902993a8f022698792502552d
Anans <19ananasik9595@mail.ru>
2023-01-27 06:26:23 +0300

    Задачи из семинара2

---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Трёхзначное число -> {number}");
+
+int res = RemoveSecondDgigit(number);
+Console.WriteLine($"Ваше число -> {res}");
+
+int RemoveSecondDgigit(int num)
+{
+    int num1 = num / 100;
+    int num3 = num % 10;
+    int result1 = num1 * 10 + num3;
+    return result1;
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,30 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+// void Multiplicity(int arg1, int arg2)
+// {
+//     if (arg1 % arg2 == 0)
+//         Console.WriteLine("Число " + arg1 + " кратно " + arg2);
+//     else
+//         Console.WriteLine("Число " + arg1 + " не кратно " + arg2 + ", остаток = " + arg1 % arg2);
+// }
+// Multiplicity(numOne, numTwo);
+
+Console.Write("Введите первое число 1: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число 2: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+int remains = Remains(numOne, numTwo);
+Console.WriteLine("кратно");
+
+int Remains(int number1, int number2)
+{
+    return number1 % number2;
+}
+
+Console.WriteLine($"не кратно, остаток {remains}");
